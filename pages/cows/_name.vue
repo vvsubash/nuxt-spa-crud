d23c5ce7895274f90049de44e23561947a1702d2
venkat <subash.venkat007@gmail.com>
2018-12-04 12:36:02 +0530

    need ud

--- pages/cows/_name.vue
+++ pages/cows/_name.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-      <ul v-for="cow in cows" v-bind:key="cow.name">
-          {{cow.belongsTo}}
-      </ul>
+    <ul v-for="cow in cows" v-bind:key="cow.name">{{cow.belongsTo}}</ul>
   </div>
 </template>
 
@@ -14,25 +12,31 @@ export default {
       cows: []
     };
   },
-  created () {
+  created() {
     // fetch the data when the view is created and the data is
     // already being observed
-    this.fetchData()
+    this.fetchData();
   },
   watch: {
     // call again the method if the route changes
-    '$route': 'fetchData'
+    $route: "fetchData"
   },
-    methods: {
-    fetchData () {
-      this.error = this.name = null
-      this.loading = true
+  methods: {
+    fetchData() {
+      this.error = this.name = null;
+      this.loading = true;
       // replace `getPost` with your data fetching util / API wrapper
-      this.$bind('cows', db.collection('users/x/cows').doc(this.$route.params.name).collection("heatData")).then(cows => {
-      this.cows === cows})
+      this.$bind(
+        "cows",
+        db
+          .collection("users/x/cows")
+          .doc(this.$route.params.name)
+          .collection("heatData")
+      ).then(cows => {
+        this.cows === cows;
+      });
     }
   }
-
 };
 </script>
 
