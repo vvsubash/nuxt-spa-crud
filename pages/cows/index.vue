9f9756c70cf6cc04a9cb198eda45a246a41240e4
venkat <subash.venkat007@gmail.com>
2018-12-29 17:34:50 +0530

    same old test

--- pages/cows/index.vue
+++ pages/cows/index.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <ul v-for="cow in cows" v-bind:key="cow.name">
-      <p>
+      <!-- <p>
         <router-link :to="'/cows/' + cow.name">{{ cow.name }}</router-link>
-        </p>
-    </ul>
-    <div class="max-w-md w-full lg:flex">
+      </p>-->
+      <div class="max-w-md w-full lg:flex">
   <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('https://tailwindcss.com/img/card-left.jpg')" title="Woman holding a mug">
   </div>
   <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
@@ -22,27 +21,68 @@
     <div class="flex items-center">
       <img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">
       <div class="text-sm">
-        <p class="text-black leading-none"></p>
+        <p class="text-black leading-none">{{cow.name}}</p>
         <p class="text-grey-dark">Aug 18</p>
       </div>
     </div>
   </div>
 </div>
+
+    </ul>
+    <div class="max-w-md w-full lg:flex">
+      <div
+        class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+        style="background-image: url('https://tailwindcss.com/img/card-left.jpg')"
+        title="Woman holding a mug"
+      ></div>
+      <div
+        class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+      >
+        <div class="mb-8">
+          <p class="text-sm text-grey-dark flex items-center">
+            <svg
+              class="fill-current text-grey w-3 h-3 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"
+              ></path>
+            </svg>
+            Members only
+          </p>
+          <div class="text-black font-bold text-xl mb-2">Can coffee make you a better developer?</div>
+          <p
+            class="text-grey-darker text-base"
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div class="flex items-center">
+          <img
+            class="w-10 h-10 rounded-full mr-4"
+            src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg"
+            alt="Avatar of Jonathan Reinink"
+          >
+          <div class="text-sm">
+            <p class="text-black leading-none"></p>
+            <p class="text-grey-dark">Aug 18</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import db from "~/plugins/firebase.js";
 export default {
-  transition: 'bounce',
+  transition: "bounce",
   data() {
     return {
       cows: null
     };
   },
   firestore: {
-    cows: db
-      .collection("users/x/cows")
+    cows: db.collection("users/x/cows")
   }
 };
 </script>
