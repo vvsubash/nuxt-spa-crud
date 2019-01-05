6328ac15e25c5bc563dd2785bdb11ffcaa1efeb7
unknown <you@example.com>
2019-01-05 17:12:43 +0530

    g

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,80 +13,95 @@ module.exports = {
   mode: 'spa',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     '~/assets/css/tailwind.css',
     '~/assets/css/main.css'
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: ['~/plugins/firebase.js'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['~/plugins/firebase.js'],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
-    // '@nuxtjs/pwa'
+    '@nuxtjs/sitemap'
   ],
 
   /*
-  ** Build configuration
-  */
- build: {
-  //  analyze: true,
-  extractCSS: true,
-  postcss: {
-    plugins: {
-      tailwindcss: path.resolve('./tailwind.js')
+   ** Build configuration
+   */
+  build: {
+    //  analyze: true,
+    extractCSS: true,
+    postcss: {
+      plugins: {
+        tailwindcss: path.resolve('./tailwind.js')
+      },
+      preset: {
+        autoprefixer: {
+          grid: true
+        }
+      }
     },
-    preset: { autoprefixer: { grid: true } }
-  },
-  extend(config, { isDev }) {
-    if (!isDev) {
-      config.plugins.push(
-        new PurgecssPlugin({
-          // purgecss configuration
-          // https://github.com/FullHuman/purgecss
-          paths: glob.sync([
-            path.join(__dirname, './pages/**/*.vue'),
-            path.join(__dirname, './layouts/**/*.vue'),
-            path.join(__dirname, './components/**/*.vue')
-          ]),
-          extractors: [
-            {
+    extend(config, {
+      isDev
+    }) {
+      if (!isDev) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            // purgecss configuration
+            // https://github.com/FullHuman/purgecss
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            extractors: [{
               extractor: TailwindExtractor,
               extensions: ['vue']
-            }
-          ],
-          whitelist: ['html', 'body', 'nuxt-progress']
-        })
-      )
+            }],
+            whitelist: ['html', 'body', 'nuxt-progress']
+          })
+        )
+      }
     }
-  }
-},
-css: ['~/assets/css/tailwind.css']
+  },
+  css: ['~/assets/css/tailwind.css']
 }
