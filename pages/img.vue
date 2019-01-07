c1dcf2e372181f1b0da4d590e53594ed09e793ff
venkat <subash.venkat007@gmail.com>
2019-01-07 22:09:52 +0530

    Merge branch 'master' of https://github.com/venkatasubash/nuxt-spa-crud

---
+++ pages/img.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-1/3">
+      <div class="flex flex-wrap">
+        <div class="w-full">
+          <img
+            src="https://picsum.photos/200
+"
+            alt
+          >
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://picsum.photos/200?image=1079
+
+"
+            alt
+          >
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://picsum.photos/200?image=1055
+
+"
+            alt
+          >
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://picsum.photos/200?image=1069
+
+"
+            alt
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped>
+</style>
