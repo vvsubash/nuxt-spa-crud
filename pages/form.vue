eedd30013f79fe3b295ffbf6620665c8765da9ad
venkat <subash.venkat007@gmail.com>
2019-04-15 13:32:51 +0530

    ookoo

--- pages/form.vue
+++ pages/form.vue
@@ -4,6 +4,9 @@
 		<br>
 		{{this.$auth.user}}
 		<br>
+		<div v-if="this.$auth.loggedIn">
+			<router-link :to="'/'+ this.$auth.user.sub +'/cows'">LinkTitle</router-link>
+		</div>
 	</div>
 </template>
 <script>
