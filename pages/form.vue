533bc592230f3a8ec0cbf02403ea671b71aac659
venkat <subash.venkat007@gmail.com>
2019-07-21 14:44:40 +0530

    dg

--- pages/form.vue
+++ pages/form.vue
@@ -2,11 +2,14 @@
   <div>
     <button @click="signIn">login</button>
     <br />
-    {{Object.keys(this.$auth)}}
-    {{this.$auth.token}}
+    {{ Object.keys(this.$auth) }}
+    {{ this.$auth.token }}
+    {{ this.$auth.$storage.getLocalStorage(token) }}
     <br />
     <div v-if="this.$auth.loggedIn">
-      <router-link :to="'/'+ this.$auth.user.sub +'/cows'">LinkTitle</router-link>
+      <router-link :to="'/' + this.$auth.user.sub + '/cows'"
+        >LinkTitle</router-link
+      >
       <button class="mx-auto bg-blue" @click="signOut">Signout</button>
     </div>
   </div>
@@ -24,15 +27,15 @@ export default {
         .loginWith("google")
         .then(result => {
           prompt(result.user.sub);
-          // firebase
-          //   .auth()
-          //   .signInWithCustomToken(token)
-          //   .catch(function(error) {
-          //     // Handle Errors here.
-          //     var errorCode = error.code;
-          //     var errorMessage = error.message;
-          //     // ...
-          //   });
+          firebase
+            .auth()
+            .signInAndRetrieveDataWithCredential(token)
+            .catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // ...
+            });
         })
         .catch(err => {
           console.log(err);
