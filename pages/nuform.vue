c92e9535b374f8a516dbb5f70801ed19d223213e
venkat <subash.venkat007@gmail.com>
2019-07-29 08:04:53 +0530

    ook

--- pages/nuform.vue
+++ pages/nuform.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <button @click="signIn">login</button>
+    <div id="firebaseui-auth-container"></div>
   </div>
 </template>
 <script>
 import firebase from "firebase";
+import * as firebaseui from "firebaseui";
+import "../node_modules/firebaseui/dist/firebaseui.css";
 export default {
-  data() {
-    return {};
-  },
-  methods: {
-    signIn() {
-      this.provider = new firebase.auth.GoogleAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(this.provider)
-        .then(result => {
-          // store the user ore wathever
-          this.$router.push("/");
-        })
-        .catch(e => {
-          this.$snotify.error(e.message);
-          console.log(e);
-        });
-    }
+  mounted() {
+    let self = this;
+
+    let uiConfig = {
+      signInOptions: [
+        {
+          provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        }
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult() {
+          localStorage.setItem("authenticated", true);
+          window.location.href = "/";
+        }
+      }
+    };
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#firebaseui-auth-container", uiConfig);
   }
 };
 </script>
